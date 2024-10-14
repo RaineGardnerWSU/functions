713e6f4b2ac5f7b184859f5722f1fad2c191dbf2
George Ray <george.ray@davistech.edu>
2024-10-14 09:53:11 -0600

    Update Sans Functions Demo File

--- demo_sans_functions.py
+++ demo_sans_functions.py
@@ -2,12 +2,13 @@
 #fahrenheit to Celsius and five from Celsius to fahrenheit.
 #Print out each calculated value.
 
+
 # Convert Celsius to Fahrenheit (five variables)
-fahrenheit1 = (100 * 1.6)+32 #Boiling
-fahrenheit2 = (0 * 1.6)+32 #Freezing
-fahrenheit3 = (30 * 1.6)+32 #hot
-fahrenheit4 = (20 * 1.6)+32 #comfortable
-fahrenheit5 = (10 * 1.6)+32 #cool
+fahrenheit1 = (100 * 1.8)+32 #Boiling
+fahrenheit2 = (0 * 1.8)+32 #Freezing
+fahrenheit3 = (30 * 1.8)+32 #hot
+fahrenheit4 = (20 * 1.8)+32 #comfortable
+fahrenheit5 = (10 * 1.8)+32 #cool
 print("Fahrenheit Temps are:")
 print(fahrenheit1)
 print(fahrenheit2)
@@ -17,14 +18,14 @@ print(fahrenheit5)
 
 #Convert Fahrenheit to Celsius only one variable
 print("Celsius Temps are:")
-celsius = (212 - 32)/1.6
+celsius = (212 - 32)/1.8 #boiling
 print(celsius)
-celsius = (32 - 32)/1.6
+celsius = (32 - 32)/1.8 #Freezing
 print(celsius)
-celsius = (100 - 32)/1.6
+celsius = (80 - 32)/1.8 #Hot
 print(celsius)
-celsius = (72 - 32)/1.6
+celsius = (72 - 32)/1.8 #Comfortable
 print(celsius)
-celsius = (65 - 32)/1.6
+celsius = (65 - 32)/1.8 #cool
 print(celsius)
 
