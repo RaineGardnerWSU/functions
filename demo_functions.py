04aa9b34a61b9f9e23c87089d10f80e72ec39505
George Ray <george.ray@davistech.edu>
2024-10-14 09:57:19 -0600

    Update Demo File

--- demo_functions.py
+++ demo_functions.py
@@ -4,28 +4,16 @@
 test = "test" #this line is here to get the debugger to stop
 
 # Convert Celsius to Fahrenheit (five variables)
-def ConvertCtoF(fahrenheit):
-    celsius = (fahrenheit * 1.8) + 32
-    return celsius
+
 
 #Convert Fahrenheit to Celsius only one variable
-def ConvertFtoC(celsius):
-    fahrenheit = (celsius - 32) / 1.8
-    return fahrenheit
+
 
 #Call the Celsius to Fahrenheit function and print
 print("Fahrenheit Temps are:")
-print(ConvertCtoF(100))
-print(ConvertCtoF(0))
-print(ConvertCtoF(30))
-print(ConvertCtoF(20))
-print(ConvertCtoF(10))
+
 
 #Call the Fahrenheit to Celsius function and print
 print("Celsius Temps are:")
-print(ConvertFtoC(212))
-print(ConvertFtoC(32))
-print(ConvertFtoC(100))
-print(ConvertFtoC(72))
-print(ConvertFtoC(65))
+
 
--- demo_sans_functions.py
+++ demo_sans_functions.py
@@ -4,28 +4,11 @@
 
 
 # Convert Celsius to Fahrenheit (five variables)
-fahrenheit1 = (100 * 1.8)+32 #Boiling
-fahrenheit2 = (0 * 1.8)+32 #Freezing
-fahrenheit3 = (30 * 1.8)+32 #hot
-fahrenheit4 = (20 * 1.8)+32 #comfortable
-fahrenheit5 = (10 * 1.8)+32 #cool
+
 print("Fahrenheit Temps are:")
-print(fahrenheit1)
-print(fahrenheit2)
-print(fahrenheit3)
-print(fahrenheit4)
-print(fahrenheit5)
+
 
 #Convert Fahrenheit to Celsius only one variable
 print("Celsius Temps are:")
-celsius = (212 - 32)/1.8 #boiling
-print(celsius)
-celsius = (32 - 32)/1.8 #Freezing
-print(celsius)
-celsius = (80 - 32)/1.8 #Hot
-print(celsius)
-celsius = (72 - 32)/1.8 #Comfortable
-print(celsius)
-celsius = (65 - 32)/1.8 #cool
-print(celsius)
+
 
