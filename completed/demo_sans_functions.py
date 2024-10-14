9ec1969ca5a281fb2d60d930ba662c303cd77d4a
George Ray <egeorgeray@gmail.com>
2024-10-14 09:54:30 -0600

    copy files to completed

---
+++ completed/demo_sans_functions.py
@@ -0,0 +1,30 @@
+#Requirement calculate 10 different values, five values from
+#fahrenheit to Celsius and five from Celsius to fahrenheit.
+#Print out each calculated value.
+
+# Convert Celsius to Fahrenheit (five variables)
+fahrenheit1 = (100 * 1.6)+32 #Boiling
+fahrenheit2 = (0 * 1.6)+32 #Freezing
+fahrenheit3 = (30 * 1.6)+32 #hot
+fahrenheit4 = (20 * 1.6)+32 #comfortable
+fahrenheit5 = (10 * 1.6)+32 #cool
+print("Fahrenheit Temps are:")
+print(fahrenheit1)
+print(fahrenheit2)
+print(fahrenheit3)
+print(fahrenheit4)
+print(fahrenheit5)
+
+#Convert Fahrenheit to Celsius only one variable
+print("Celsius Temps are:")
+celsius = (212 - 32)/1.6
+print(celsius)
+celsius = (32 - 32)/1.6
+print(celsius)
+celsius = (100 - 32)/1.6
+print(celsius)
+celsius = (72 - 32)/1.6
+print(celsius)
+celsius = (65 - 32)/1.6
+print(celsius)
+
