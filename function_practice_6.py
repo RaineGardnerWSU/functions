52d1c9bf6f2fc947e5533d23d0c69c3889ed3b2c
George Ray <george.ray@davistech.edu>
2024-10-15 08:50:40 -0600

    Add practice 6

---
+++ function_practice_6.py
@@ -0,0 +1,14 @@
+#Check out this link for Default Argument Values
+#https://docs.python.org/3.13/tutorial/controlflow.html#keyword-arguments
+
+
+#6. Create a function that takes four keyword arguments and tell a story with it.
+#  Function Name: "Story"
+#  Parameter(s): studentName, teacherName, schoolName, sport
+#  Return: string
+#  Example Input 1: "George", "Dr. Valle", "Weber State", "Football"
+#  Example Return 1: "I Love Football. I went to the Weber State Football game. I saw Dr. Valle there. 
+#       He was not cheering for Weber State. He said, "don't tell anyone George."
+
+#Define the #5 function here
+
